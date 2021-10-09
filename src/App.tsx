051490f0339796f76e9ca94b14a71d74c2e14c2b
Higor Campelo <higorxadrez@hotmail.com>
2021-10-09 07:06:05 -0300

    Creation of the type item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import * as C from './App.styles';
+import { useState } from 'react';
+import { Item } from './types/itemList';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {
+      id: 1,
+      name: 'Estudar ReactJS',
+      done: false
+    },
+    {
+      id: 2,
+      name: 'Estudar 1 hora de Inglês',
+      done: false
+    }
+  ]);
+
   return (
     <C.Container>
       <C.Area>
         <C.Header> Lista de Atividades </C.Header>
+
+        {/* Area de adicionar nova tarefa*/}
+        {list.map((item, index) => (
+          <div> {item.name}</div>
+        ))}
+
       </C.Area>
     </C.Container>
 
